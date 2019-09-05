8ffc4760f658fead2d21807f06a6583db48acdc3
Nitin Agrawal <agrawalnitin12@gmail.com>
2019-09-05 12:49:04 +0530

    BDD framework updated with contactStepDefinitionfile

---
+++ src/main/java/stepDefinitions/ContactsSetpDefinition.java
@@ -0,0 +1,12 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Given;
+
+public class ContactsSetpDefinition {
+	
+	@Given("^This is valid login test$")
+	public void this_is_valid_login_test() {
+
+	}
+
+}
